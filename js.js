8429b995573bf7c90fa995b2bbbd36908a85d5fc
vadim <example>
2019-06-16 18:14:32 +0200

    comit

---
+++ js.js
@@ -0,0 +1,49 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(4/0);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.png','apple.bmp'];
+
+console.log(arr[0])
+
+// alert("Hello World!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
